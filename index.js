653dc5bc6a578db2249f3b3acb70ee995a9f3c6a
Divya Dave <divyadave1205@gmail.com>
2025-04-18 20:45:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 
 // For use .env file's variables in this file
 require('dotenv').config();
@@ -10,10 +9,6 @@ const app = express();
 // Middleware
 app.use(express.json());
 
-// Use CORS middleware
-app.use(cors({
-    'origin': 'http://localhost:5122',
-}));
 
 // Routes
 const noteRoutes = require('./routes/notes');
@@ -23,8 +18,8 @@ app.use('/api/notes', noteRoutes);
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+    app.listen(process.env.PORT||3000, () => {
+      console.log(`Server running on port ${process.env.PORT||3000}`);
     });
   })
   .catch(err => console.error("Mongo error:", err));
